5934df241bfe18b1e7c4474e6c1eb40a25552b9c
Dominik Zogg <dominik.zogg@ikmail.com>
2025-08-27 07:22:45 +0200

    cs-fix

--- src/decoder/index.ts
+++ src/decoder/index.ts
@@ -2,5 +2,5 @@
  * @deprecated This file re-exports the old API. Use './decoder.ts' directly instead.
  */
 
-export { DecodeError, createDecoder} from './decoder.js';
+export { DecodeError, createDecoder } from './decoder.js';
 export type { TypeDecoder, Decoder } from './decoder.js';
